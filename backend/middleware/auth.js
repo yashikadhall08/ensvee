ca6e0e2e4018afcdb46f0e55f397c9f9367fe5b3
yashikadhall08 <52121607+yashikadhall08@users.noreply.github.com>
2021-11-13 02:10:11 +0530

    auth

---
+++ backend/middleware/auth.js
@@ -0,0 +1,19 @@
+
+ const jwt = require("jsonwebtoken")
+ const config = require("../config/auth.config")
+ 
+ authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) return res.sendStatus(401)
+  
+    jwt.verify(token, config.secret, (err, user) => {
+      console.log(err)
+      if (err) return res.sendStatus(403)
+      req.user = user
+      next()
+    })
+  }
+
+
+  module.exports = authenticateToken;
